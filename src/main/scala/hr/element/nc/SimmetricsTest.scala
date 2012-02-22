771adfebe0c23ad1dcba906987d59b7806ba1bb9
Ana Marjanica <ana@element.hr>
2012-02-22 16:57:19 +0100

    Changes to SimmetricsTest

--- src/main/scala/hr/element/nc/SimmetricsTest.scala
+++ src/main/scala/hr/element/nc/SimmetricsTest.scala
@@ -6,26 +6,36 @@ import scala.util.parsing.combinator._
 import scala.util.parsing.combinator.syntactical._
 import scala.io.Source
 import java.io.File
+import java.text.Normalizer
+import java.util.regex.Pattern
 
 
 object Test {
   def main(args: Array[String]): Unit = {
-    val names = Seq("ana","Ana", "Bačić", "a na", "Bachich", "bacic")
-
     val csv    = readFile("../name-compare/imena.csv")
-    val parsedList = parseCsv(csv)
-    val countNames = parsedList.size
-    println(countNames)
-
-    matchNames(new Levenshtein, parsedList.tail.take(50))//ok
-//    matchNames(new ChapmanLengthDeviation, parsedList.tail.take(50))//ne valja
-//    matchNames(new ChapmanOrderedNameCompoundSimilarity, parsedList.tail.take(50))//ok
-//    matchNames(new Jaro, parsedList.tail.take(50))//ne valja
-//    matchNames(new NeedlemanWunch, parsedList.tail.take(50))//ok
-//    matchNames(new SmithWatermanGotoh, parsedList.tail.take(50))//tak tak
-//    matchNames(new SmithWatermanGotohWindowedAffine, parsedList.tail.take(50))//tak tak
+    val parsedList  = parseCsv(csv)
+    val countNames  = parsedList.size
+    val optParsList = optimizedList(parsedList.tail.take(300))
 
 
+//    matchNames(new Levenshtein, optParsList)//ok
+//    getMatchedNames(new Levenshtein, optParsList, 0.7f) foreach println
+////    matchNames(new ChapmanOrderedNameCompoundSimilarity, optParsList)//ok
+//    getMatchedNames(new ChapmanOrderedNameCompoundSimilarity, optParsList, 0.7f) foreach println
+////    matchNames(new Jaro, optParsList)//bzvz
+//    getMatchedNames(new Jaro, optParsList, 0.7f) foreach println
+////    matchNames(new NeedlemanWunch, optParsList)//ok
+//    getMatchedNames(new NeedlemanWunch, optParsList, 0.7f) foreach println
+////    matchNames(new SmithWatermanGotoh, optParsList)//ok je
+//    getMatchedNames(new SmithWatermanGotoh, optParsList, 0.7f) foreach println
+////    matchNames(new SmithWatermanGotohWindowedAffine, optParsList)//ok je
+//    getMatchedNames(new SmithWatermanGotohWindowedAffine, optParsList, 0.7f) foreach println
+
+
+  }
+  def getMatchedNames(similarityMethod: AbstractStringMetric, names: Seq[String], correlationMin: Float) = {
+    val res = testSimilarity(similarityMethod, names)
+    res.map( e => (e._1, e._2.filter(_._1 > correlationMin)))
   }
   def matchNames(similarityMethod: AbstractStringMetric, names: Seq[String]){
     println("%s similarity check: \n" format similarityMethod.getShortDescriptionString)
@@ -43,8 +53,17 @@ object Test {
     simListSorted
   }
 
-  //def matchByName
   //def matchByNameAndSurname
+  def optimizedList(list: Seq[String]) = {
+    list.map( name => removeDiacritics(name.trim().toLowerCase.replace("\"","").replace(","," ")))
+  }
+
+  //Jedino đ ne miče i valjda slična slova koja se ne sastoje od 2 dijela
+  private def removeDiacritics(str: String) = {
+    val nfd = Normalizer.normalize(str, Normalizer.Form.NFD)
+    val pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+")
+    pattern.matcher(nfd).replaceAll("")
+    }
 
 
 }
